8d0cd822ca6ae82be1f0a9da5d4f15adf0d8e54d
mattgreenawalt <mgreeny@terpmail.umd.edu>
2023-03-27 22:25:50 -0400

    Added app route for Base, Contact, and Favorite Course routes. Added bootstrap inheritance from Base html to child Contact and Favorite Course pages. Added form GET and POST functionality to Contact page and URL passing to Favorite Course page. Added app routes to app.py file.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -15,9 +15,20 @@ def hello():  # put application's code here
 def about():  # put application's code here
     return render_template('about.html')
 
-@app.route('/favorite-courses')
-def favoritecourses(): # put code here
-    return render_template('favorite-courses.html')
+@app.route('/favorite-course', methods=['GET', 'POST'])
+def favoritecourse():
+    if request.method == 'POST':
+        print('Course Subject: ' + request.args.get('course_subject'))
+        print('Course Code: ' + request.args.get('course_code'))
+
+    return render_template('favorite-course.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run()
